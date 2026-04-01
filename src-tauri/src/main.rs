02a8f6e37d3c5034c3d87f416b06a339280ce2ab
retrogramx <aalvarez0295@gmail.com>
2026-04-01 04:37:34 -0700

    feat: add global hotkey registration for Ctrl+Shift+G/D/E

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -1,5 +1,8 @@
 #![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
 
+use tauri::{Emitter, Manager};
+use tauri_plugin_global_shortcut::{Code, GlobalShortcutExt, Modifiers, Shortcut};
+
 mod profiles;
 
 #[tauri::command]
@@ -24,12 +27,35 @@ fn delete_profile(name: String) -> Result<(), String> {
 
 fn main() {
     tauri::Builder::default()
+        .plugin(tauri_plugin_global_shortcut::Builder::new().build())
         .invoke_handler(tauri::generate_handler![
             save_profile,
             load_profile,
             list_profiles,
             delete_profile,
         ])
+        .setup(|app| {
+            let toggle = Shortcut::new(Some(Modifiers::CONTROL | Modifiers::SHIFT), Code::KeyG);
+            let complete = Shortcut::new(Some(Modifiers::CONTROL | Modifiers::SHIFT), Code::KeyD);
+            let expand = Shortcut::new(Some(Modifiers::CONTROL | Modifiers::SHIFT), Code::KeyE);
+
+            let app_handle = app.handle().clone();
+            app.global_shortcut().on_shortcut(toggle, move |_, _, _| {
+                let _ = app_handle.emit("hotkey-toggle", ());
+            })?;
+
+            let app_handle = app.handle().clone();
+            app.global_shortcut().on_shortcut(complete, move |_, _, _| {
+                let _ = app_handle.emit("hotkey-complete", ());
+            })?;
+
+            let app_handle = app.handle().clone();
+            app.global_shortcut().on_shortcut(expand, move |_, _, _| {
+                let _ = app_handle.emit("hotkey-expand", ());
+            })?;
+
+            Ok(())
+        })
         .run(tauri::generate_context!())
         .expect("error while running tauri application");
 }
